cba11398f784a876d8bccf877031ef379a7fe40e
zero86 <katanazero86@gmail.com>
2020-03-10 20:49:58 +0900

    [feature] 테스트
    - dotenv
    - express-session
    - jsonwebtoken
    3가지 npm 모듈 추가

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+dotenv.config();
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +21,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// set the secret key variable for jwt
+app.set('jwt-secret', process.env.secret);
+
+// index page, just for testing
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
