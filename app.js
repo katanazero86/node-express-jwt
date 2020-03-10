8ce7efe7fd0db1d5408fbbcca584428457bb7783
zero86 <katanazero86@gmail.com>
2020-03-10 21:37:37 +0900

    [feature] 테스트
    - 코드 작업중..

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
+const session = require(`express-session`);
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -9,6 +10,7 @@ dotenv.config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const apiRouter = require('./routes/api/jwtApi');
 
 const app = express();
 
@@ -21,14 +23,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret : process.env.sessionSecret,
+  resave: false,
+  saveUninitialized: true
+}));
 
-// set the secret key variable for jwt
 app.set('jwt-secret', process.env.secret);
-
-// index page, just for testing
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api/jwtApi.js
+++ routes/api/jwtApi.js
@@ -1,1 +1,59 @@
 const jwt = require('jsonwebtoken');
+const express = require('express');
+const router = express.Router();
+
+// jwt encode
+router.post('/encode', async (req, res) => {
+
+    const result = await asyncEncode(req);
+
+    if(result.message === 'success') {
+        res.send(200);
+    } else {
+        console.log(result.message);
+        res.send(400);
+    }
+
+});
+
+// jwt decode
+router.get('/decode', (req, res) => {
+    const session = req.session;
+    console.log(session);
+
+    res.send(200);
+});
+
+// jwt 삭제
+router.delete('/destroy', (req, res) => {
+
+});
+
+
+const asyncEncode = (req) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const secret = req.app.get('jwt-secret');
+
+            // default HMAC SHA256
+            // console.log(req.body); {"name" : "zero86"}
+            const token = jwt.sign(req.body, secret);
+            const session = req.session;
+
+            session[token] = true;
+
+            resolve({message : 'success'});
+        } catch(error) {
+            reject({message : error});
+        }
+    });
+};
+
+const asyncDecode = (req) => {
+    return new Promise((resolve, reject) => {
+
+    });
+};
+
+
+module.exports = router;
